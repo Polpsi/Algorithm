e561e4ed2f4ab5aa66b854c886387360460f14f7
Serg <psi_gb@mail.ru>
2020-11-27 15:39:39 +0300

    Lesson2Before

---
+++ Lesson2/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        MyArrayList<Integer> mal = new MyArrayList<>(20);
+        for (int i = 0; i < 10; i++) {
+            mal.add(random.nextInt(100));
+        }
+
+    }
+}
